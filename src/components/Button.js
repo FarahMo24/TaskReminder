8a3a453739483f08ae9111035da91829f1958f1a
Mohamed <farahmo242@gmail.com>
2021-08-09 17:23:34 -0700

    json backend completed

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,33 @@
-const Button = () => {
-  return (<button className='btn'>Add</button>)
+import PropTypes from 'prop-types'
+
+
+const Button = ({color,text, onClick}) => {
+  return (
+  <button 
+    onClick = {onClick}
+    style = {{backgroundColor:color}} 
+    className='btn'>
+    
+    {text}
+    
+    </button>
+  
+  )
 }
+
+// Default prop
+Button.defaultProps = {
+  color:'steelblue'
+
+}
+
+// Proptypes
+Button.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
+}
+
+
 
 export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,30 @@
 import PropTypes from 'prop-types'
+import {useLocation} from 'react-router-dom'
 import Button from './Button'
 
-const Header = ({title}) => {
+const Header = ({title, onAdd, showAdd}) => {
+ const location = useLocation()
   return (
 
     <header className = 'header'>
       <h1>{title}</h1>
-      <Button/>
+      {
+       location.pathname == '/' && (  
+        <Button 
+          color = {showAdd ? 'red':'green'} 
+          text={showAdd ? "Close" : "Add"} 
+          onClick={onAdd}
+        />
+
+       )} 
+      
     </header>
   )
 }
 
 // Default prop
 Header.defaultProps = {
-  title: 'Task Tracker'
+  title: 'Task Reminder'
 }
 
 // Proptypes
