dde511340a6af8451910ebaa7952ca76e24c74cb
Mohamed Farah <47758699+FarahMo24@users.noreply.github.com>
2021-08-30 16:38:20 -0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = ({title, onAdd, showAdd}) => {
     <header className = 'header'>
       <h1>{title}</h1>
       {
-       location.pathname == '/' && (  
+       location.pathname === '/' && (  
         <Button 
           color = {showAdd ? 'red':'green'} 
           text={showAdd ? "Close" : "Add"} 
